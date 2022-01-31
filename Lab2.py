e028cb5e81dcfb0366d0fa4ca3e415b36e539dbc
EC2 Default User <ec2-user@ip-172-31-64-230.ec2.internal>
2022-01-31 21:26:28 +0000

    Lab2

---
+++ Lab2.py
@@ -0,0 +1,27 @@
+my_name = 'Tom'
+print(my_name.upper())
+
+my_id = 123
+print(my_id)
+
+my_id = your_id = 123
+print(my_id)
+print(your_id)
+
+my_id_str = "123"
+print(my_id_str)
+
+#print(my_name + my_id)
+
+print(my_name + my_id_str)
+
+print(my_name * 3)
+
+print('hello world. This is my first python string.'.split('.'))
+
+message = "Tom's ID is 123."
+print(message)
+
+git add--all
+git commit -m"my Lab2.py file"
+git push
